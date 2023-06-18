6839e542ddff37687426d56836e998fdb707d689
riley <nrileyfletcher@gmail.com>
2023-06-17 20:11:38 -0400

    ll

--- linkedlist/linkedlist.c
+++ linkedlist/linkedlist.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct node;
+
+struct list {
+	struct node *head;
+};
+
+struct node {
+	int data;
+	struct node *next;
+};
 
 /* Implement linked list struct */
 int main() {
+	printf("Test");
 	return 0;
 }
