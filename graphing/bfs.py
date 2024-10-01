9ae9352716c4a8af52533666258f290100c7ab8a
riley <89424122+nrfletcher@users.noreply.github.com>
2024-10-01 14:57:20 -0400

    bfs

--- graphing/bfs.py
+++ graphing/bfs.py
@@ -9,8 +9,29 @@ class BFSGraph(ListGraph):
     def __init__(self, node_count: int) -> None:
         super().__init__(node_count)
 
-    def bfs(self):
-        pass
+    def bfs(self, source: int):
+        
+        visited = [0 for i in range(len(self.edges))]
+        queue = []
+        queue.append(source)
+
+        while len(queue) > 0:
+            node = queue.pop(0)
+
+            if not visited[node]:
+                print(node)
+                visited[node] = 1
+
+            for neighbor in self.edges[node]:
+                if visited[neighbor] == 0:
+                    queue.append(neighbor)
 
 if __name__ == "__main__":
-    pass
+    g = BFSGraph(5)
+    g.add_edge(0, 1)
+    g.add_edge(0, 2)
+    g.add_edge(1, 3)
+    g.add_edge(2, 4)
+    g.add_edge(3, 4)
+    g.add_edge(1, 2)
+    g.bfs(0)
